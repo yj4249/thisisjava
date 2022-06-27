a7f7ecb8dd306cedc9ac9915cc2cfd474a630176
yj4249 <bad9uest@gmail.com>
2022-06-27 12:42:02 +0900

    LongExample

---
+++ chap02/src/sec02/exam04_long/LongExample.java
@@ -0,0 +1,17 @@
+package sec02.exam04_long;
+
+public class LongExample {
+	public static void main(String [] args) {
+		long var1 = 10;
+		long var2 = 20L;
+	//	long var3 = 10000000000000
+		long var4 = 10000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var4);
+				
+	}
+	
+}
+// 롱변수는 뒤에 L을 붙여줘야 정상적으로 컴파일됨.
