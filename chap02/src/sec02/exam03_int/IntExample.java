3c760e670ca98e7efdda57e657122fc05905e4eb
yj4249 <bad9uest@gmail.com>
2022-06-27 12:28:03 +0900

    intExample

---
+++ chap02/src/sec02/exam03_int/IntExample.java
@@ -0,0 +1,15 @@
+package sec02.exam03_int;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+		int var1 = 10;
+		int var2 = 012;
+		int var3 = 0xA;
+		
+		System.out.println(var1);
+		System.out.println(var1);
+		System.out.println(var1);
+	}
+
+}
