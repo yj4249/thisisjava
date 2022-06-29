58f71e0bd0abf726965ea882e00ce90edbc49412
yj4249 <bad9uest@gmail.com>
2022-06-29 09:47:58 +0900

    CharOperator

---
+++ chap03/src/sec04/exam01_arithmetic/CharOperatorExample.java
@@ -0,0 +1,15 @@
+package sec04.exam01_arithmetic;
+
+public class CharOperatorExample {
+
+	public static void main(String[] args) {
+		char c1 = 'A' + 1;
+		char c2 = 'A';
+		//char c3 = c2 +1;    컴파일에러
+		System.out.println("c1:" + c1);
+		System.out.println("c2:" + c2);
+		//System.out.println("c3:" + c3);
+		//char의 타입은 정수
+	}
+
+}
