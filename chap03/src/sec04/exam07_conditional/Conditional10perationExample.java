ae23e94c605d33560e3f221b43c5d749ea0a888a
yj4249 <bad9uest@gmail.com>
2022-06-29 13:02:50 +0900

    Conditional

---
+++ chap03/src/sec04/exam07_conditional/Conditional10perationExample.java
@@ -0,0 +1,12 @@
+package sec04.exam07_conditional;
+
+public class Conditional10perationExample {
+
+	public static void main(String[] args) {
+		int score = 85;
+		char grade = (score > 90) ? 'A' : ((score > 80) ? 'B' : 'C' );
+		System.out.println(score + "점은" + grade + "등급입니다." );
+
+	}
+
+}
