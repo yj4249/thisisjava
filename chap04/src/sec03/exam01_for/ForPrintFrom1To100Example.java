ca5b6c279a70f360442eb3fdb7efebd08ce416a7
yj4249 <bad9uest@gmail.com>
2022-06-30 12:06:03 +0900

    For

---
+++ chap04/src/sec03/exam01_for/ForPrintFrom1To100Example.java
@@ -0,0 +1,11 @@
+package sec03.exam01_for;
+
+public class ForPrintFrom1To100Example {
+	public static void main(String[] args) {
+		int i = 1;
+		for(i=1; i<10; i++)
+			System.out.println(i);
+	}	
+	
+
+}
---
+++ chap04/src/sec03/exam01_for/ForSumFrom1To10Example.java
@@ -0,0 +1,23 @@
+package sec03.exam01_for;
+
+public class ForSumFrom1To10Example {
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		for(int i=1; i<=100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1~100гу : " + sum);
+		
+/*		int sum = 0;
+		
+		int i = 0;
+		for( i=1; i<=100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1~" + (i-1) + "гу : " + sum);
+		*/
+	}
+}
